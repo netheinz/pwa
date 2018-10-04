0e54b16dea74bf3aee6f678e1e6118a3888738b8
heka <heka@techcollege.dk>
2018-10-04 13:58:24 +0200

    pwa

---
+++ public_html/assets/js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Installer service worker hvis nødvendigt
+ */
+if('serviceWorker' in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(registration => {
+                console.log("Service Worker registred");
+            })
+            .catch(error => {
+                console.error("Registration failed!");
+            })
+    })
+}
---
+++ public_html/sw.js
@@ -0,0 +1,33 @@
+/**
+ * Installer Service Worker
+ * Lytter efter install event og tilføjer App Shell filer til cache
+ */
+self.addEventListener("install", function(e) {
+    console.log("Service Worker Installed");
+    e.waitUntil(
+        caches.open('myapp').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.htm',
+                '/assets/css/inline.css',
+                '/assets/js/app.js'
+            ]);
+        })
+    );
+});
+
+/**
+ * SW Fetch - if no match in cache fetch from network and add to cache
+ */
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.open('songscape').then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                return response || fetch(event.request).then(function(response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
